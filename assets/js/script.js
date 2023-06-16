170e47bcbc682673ba1d46b0d3b2a2df0785b984
kelllyy101 <>
2023-06-16 21:21:20 +0000

    add functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function startGame() {
+
+}
+
+function setNextQuestion() {
+
+}
+
+function chooseAnswer() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+const questions = [
+    {
+        question: '',
+        answer: [
+            { text: '', correct: true },
+            { text: '', correct: false },
+            { text: '', correct: false },
+            { text: '', correct: false },
+        ]
+    }
+];
