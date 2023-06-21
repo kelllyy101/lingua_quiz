38336e254ffbe267e91936d3bea727c50f387db5
kelllyy101 <>
2023-06-21 13:23:08 +0000

    add game variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,19 @@ const questions = [
         ]
     }
 ];
+
+document.addEventListener('DOMContentLoaded', function () {
+    runMainScreen();
+});
+
+/**
+ * Set up of game variables to vary display/hide
+ */
+let mainLoginScreen = document.getElementById("login-screen");
+let getInstructions = document.getElementById("instructions-icon");
+let displayGuessNumber = document.getElementById("guesses");
+let errorMessage = document.getElementById("error-message");
+let chooseLevelScreen = document.getElementById("choose-level-screen");
+let gameScreen = document.getElementById("game-screen");
+let correctScreen = document.getElementById("correct-screen");
+let wrongScreen = document.getElementById("wrong-screen");
