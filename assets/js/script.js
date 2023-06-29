d5468b9a834bbb5644314e2ecc633c5108eb38c2
kelllyy101 <>
2023-06-29 13:38:40 +0000

    brought event listeners to the bottom and defined questions earlier

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,21 @@ const userInputElement = document.getElementById('user');
 const userLogButton = document.getElementById('user-log');
 const chooseLevelScreen = document.getElementById('choose-level-screen');
 const levelButtons = document.querySelectorAll('.button-level');
+const difficultyBeginnerBtn = document.getElementById('beginner-btn');
+const difficultyIntermediateBtn = document.getElementById('intermediate-btn');
+const difficultyAdvancedBtn = document.getElementById('advanced-btn');
+
+
 
 // Function to handle user log button click
 document.getElementById("user-log").addEventListener("click", checkUsername);
 
 function checkUsername() {
     let username = document.getElementById("user").value.trim();
-
+    let errorMessage = document.getElementById("error-message");
     if (username.length >= 1 && username.length <= 12) {
         chooseLevelScreen.style.display = "block";
         mainLoginScreen.style.display = "none";
-        document.getElementById("user-icon").style.display = "block";
         document.getElementById("username").innerText = username;
     } else {
         errorMessage.style.display = "block";
@@ -49,106 +53,11 @@ levelButtons.forEach(button => {
     button.addEventListener('click', handleLevelButtonClick);
 });
 
-function selectGameLevel() {
-    score = 0;
-    document.getElementById('level-buttons').addEventListener('click', function (event) {
-        if (!event.target.className.includes("button-level")) return; // prevent click over all div with three buttons
-        let button = event.target;
-        let gameLevel = button.getAttribute('data-type');
-        setGame(gameLevel);
-    });
-}
-selectGameLevel();
+handleLevelButtonClick();
 
-/**
- * Set the game screen based on level selected.
- * Display selected game level, number of guesses left, high score and score.
- * Set up category and a phrase to be guessed.
- * Populate underscores for hidden phrase and keyboard buttons.
- * @param {string} gameLevel 
- */
-function setGame(gameLevel) {
-    document.getElementById("difficulty-level").innerHTML = `${gameLevel}`;
-    document.getElementById("high-score").innerHTML = highScore;
-    document.getElementById("score").innerHTML = score;
-}
-// ABOVEEEEEEEEE
-
-
-let shuffledQuestions, currentQuestionIndex;
-
-startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++;
-    setNextQuestion();
-});
-
-function startGame() {
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove('hide');
-    setNextQuestion();
-}
-
-function setNextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn');
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener('click', selectAnswer);
-        answerButtonsElement.appendChild(button);
-    });
-}
-
-function resetState() {
-    clearStatusClass(document.body);
-    nextButton.classList.add('hide');
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
-
-function selectAnswer(e) {
-    const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct;
-    setStatusClass(document.body, correct);
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct);
-    });
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide');
-    } else {
-        startButton.innerText = 'Restart';
-        startButton.classList.remove('hide');
-    }
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-        element.classList.add('correct');
-    } else {
-        element.classList.add('wrong');
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct');
-    element.classList.remove('wrong');
-}
 
 //Questions
-const questions = [
+const beginnerQuestions = [
     {
         question: "Which word is a noun?",
         answers: [
@@ -275,3 +184,110 @@ window.addEventListener('scroll', () => {
         footer.classList.remove('show-footer');
     }
 });
+
+
+
+function selectGameLevel(event) {
+    let difficulty = event.target.getAttribute('data-type');
+
+    if (difficulty === "beginner") {
+        shuffledQuestions = beginnerQuestions.sort(() => Math.random() - 0.5);
+        currentQuestionSet = shuffledQuestions;
+        playerDifficulty = "beginner";
+        difficultyEasyBtn.innerText = "Loading...";
+    } else if (difficulty === "intermediate") {
+        shuffledQuestions = intermediateQuestions.sort(() => Math.random() - 0.5);
+        currentQuestionSet = shuffledQuestions;
+        playerDifficulty = "intermediate";
+        difficultyIntermediateBtn.innerText = "Loading...";
+    } else if (difficulty === "advanced") {
+        shuffledQuestions = advancedQuestions.sort(() => Math.random() - 0.5);
+        currentQuestionSet = shuffledQuestions;
+        playerDifficulty = "advanced";
+        difficultyAdvancedBtn.innerText = "Loading...";
+    }
+
+};
+selectGameLevel();
+
+
+// ABOVEEEEEEEEE- ADDED diff let var startQuiz 
+
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
+function startGame() {
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'Restart';
+        startButton.classList.remove('hide');
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
+
+// Event Listeners
+difficultyBeginnerBtn.addEventListener("click", selectGameLevel);
+difficultyIntermediateBtn.addEventListener("click", selectGameLevel);
+difficultyAdvancedBtn.addEventListener("click", selectGameLevel);
+
