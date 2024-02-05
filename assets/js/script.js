7cd3c361fc4775a69493deb5cb36217ad397f039
kelllyy101 <kellyom81@hotmail.com>
2024-02-05 18:37:14 +0000

    countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ const questionContainer = document.getElementById('question-box');
 const questionContainerElement = document.getElementById('question-container');
 const scoreMessage = document.getElementById('score-message');
 let timeLeft = document.querySelector(".time-left");
-let count = 10;
+let count = 15;
 let countdown;
 document.getElementById("user-log").addEventListener("click", checkUsername);
 
@@ -443,7 +443,7 @@ function resetState() {
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
-    count = 10;
+    count = 15;
 }
 
 let correctCounter, wrongCounter;
